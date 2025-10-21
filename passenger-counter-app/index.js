1eb00d54977cdc31fbac7587e895edc6165a53c5
amogharajsandur <amogharaj.sandur@yahoo.com>
2025-10-22 00:01:00 +0530

    feat: Implemented Save/Reset Value functions + Basic CSS Styling

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,15 +1,20 @@
 let value = 0;
 
 let count = document.getElementById("count");
+let body = document.getElementById("body");
 let saved = document.getElementById("saved");
 
 let increaseBtn = document.getElementById("increase");
 let decreaseBtn = document.getElementById("decrease");
 let saveBtn = document.getElementById("save");
+let resetCountBtn = document.getElementById("reset");
+let resetSavedBtn = document.getElementById("reset-saved");
 
 increaseBtn.addEventListener("click", increase);
 decreaseBtn.addEventListener("click", decrease);
-saveBtn.addEventListener("click", save(value));
+saveBtn.addEventListener("click", save);
+resetCountBtn.addEventListener("click", resetCount);
+resetSavedBtn.addEventListener("click", resetSaved);
 
 function increase() {
     value += 1;
@@ -23,11 +28,28 @@ function decrease() {
 
 function updater(value) {
     count.innerText = value;
-    console.log(count.innerText);
+    // console.log(count.innerText);
 }
 
-function save(value) {
-    saved.innerText += `- ${value}`;
+let savedValues = [];
+
+function save() {
+    // if (savedValues) {
+        savedValues += `${value}, `
+        saved.innerText = savedValues;
+    // } else {
+    //     alert("Nothing to save.")
+    // }
 }
 
-console.log(count.innerText);
+function resetCount() {
+    value = 0;
+    updater(value);
+}
+
+function resetSaved() {
+    savedValues = [];
+    saved.innerText = savedValues;
+}
+
+// console.log(count.innerText);
