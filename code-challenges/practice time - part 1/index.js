c5dcc29024f34ca4cbd9007d721e8628d929461c
amogharajsandur <amogharaj.sandur@yahoo.com>
2025-10-23 17:15:43 +0530

    Practice Time - part 1 coding challenge initiated

---
+++ code-challenges/practice time - part 1/index.js
@@ -0,0 +1,25 @@
+// ===========
+
+/*
+let name = "Linda";
+let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+function clg() {
+    console.log(greeting + ", " + name + "!");
+}
+
+clg();
+*/
+
+// ===========
+
+/*
+let firstName = "Amogha";
+let lastName = "Raj";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+*/
